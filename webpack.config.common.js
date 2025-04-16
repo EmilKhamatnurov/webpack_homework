a5123ad0a85abbc583677cec7fba085deb2a7e73
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2025-04-17 01:40:54 +0500

    build(code): Добавлен плагин для оптимизации изображений

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const BUILD_FOLDER = 'dist';
+const BUILD_FOLDER = "dist";
 
 module.exports = {
-	context: path.resolve(__dirname, 'src'),
-	entry: './index.js',
+	context: path.resolve(__dirname, "src"),
+	entry: "./index.js",
 	output: {
-		filename: '[name].[contenthash].js',
+		filename: "[name].[contenthash].js",
 		path: path.resolve(__dirname, BUILD_FOLDER),
 		clean: true,
 		// assetModuleFilename: 'images/[hash][ext][query]',
@@ -18,7 +18,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
 			},
 			{
 				test: /\.s[ac]ss$/i,
@@ -26,38 +26,38 @@ module.exports = {
 					// Creates `style` nodes from JS strings
 					MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
-					'css-loader',
+					"css-loader",
 					{
-						loader: 'postcss-loader',
+						loader: "postcss-loader",
 						options: {
 							postcssOptions: {
-								plugins: [require('postcss-preset-env')],
+								plugins: [require("postcss-preset-env")],
 							},
 						},
 					},
 					// Compiles Sass to CSS
-					'sass-loader',
+					"sass-loader",
 				],
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: 'fonts/[name][ext]',
+					filename: "fonts/[name][ext]",
 				},
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: 'files/images/[name][ext]',
+					filename: "files/images/[name][ext]",
 				},
 			},
 			{
 				test: /\.(mp3|wav|ogg)$/i, // Поддержка аудиофайлов
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: 'files/audio/[name][ext]',
+					filename: "files/audio/[name][ext]",
 				},
 			},
 		],
@@ -65,12 +65,12 @@ module.exports = {
 	plugins: [
 		new MiniCssExtractPlugin(),
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'public/index.html'),
+			template: path.resolve(__dirname, "public/index.html"),
 		}),
 		new CopyPlugin({
 			patterns: [
 				{
-					from: path.resolve(__dirname, 'public/favicon.ico'),
+					from: path.resolve(__dirname, "public/favicon.ico"),
 					to: path.resolve(__dirname, BUILD_FOLDER),
 				},
 			],
